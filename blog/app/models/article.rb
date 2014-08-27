e73821e57322e2c3d9110a55e27679563149cddb
Justine Jones <justinejones91@gmail.com>
2014-08-27 18:27:29 -0400

    fixed the associaitons

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ActiveRecord::Base
 	belongs_to :user
-	has_many :comments through :users
+	has_many :comments 
+	has_many :users :through => :comments
 
-	
 	validates :title, presence: true,
 										length: { minimum: 5 }
 end
