9099829e3ed583ca69f0a942f6fa924a136a2817
Justine Jones <justinejones91@gmail.com>
2014-08-27 17:44:30 -0400

    created comment model

---
+++ blog/db/migrate/20140827213754_add_user_id_column_to_article_table.rb
@@ -0,0 +1,5 @@
+class AddUserIdColumnToArticleTable < ActiveRecord::Migration
+  def change
+  	add_column :articles, :user_id, :integer
+  end
+end
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,11 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618205126) do
+ActiveRecord::Schema.define(version: 20140827214005) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  create_table "comments", force: true do |t|
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "description"
+    t.string   "url"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
