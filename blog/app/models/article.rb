e55aa304b4d609387efa28b1b572407d5cd5c17c
Justine Jones <justinejones91@gmail.com>
2014-08-27 19:56:30 -0400

    fixed a syntax error

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments 
-	has_many :users :through => :comments
+	has_many :users, :through => :comments
 
 	validates :title, presence: true,
-										length: { minimum: 5 }
+		validates	 :title, length: { minimum: 6 }
 end
