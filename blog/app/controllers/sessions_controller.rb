190444903a95b39af5964faae2d09ccc5be1f4d5
Justine Jones <justinejones91@gmail.com>
2014-08-27 17:56:09 -0400

    added create and destroy session actions

--- blog/app/controllers/sessions_controller.rb
+++ blog/app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+	def new
+		
+	end
+
+	def create
+		user = User.find_by_email(params[:emai])
+
+		if user && user.authenticate(params[:password])
+		session[:user_id] = user.id
+		redirect_to articles_url
+		else
+			flash.now[:alert] = "Invalid email or password"
+			render :new
+	end
+
+	def destroy
+		sessio[:user_id] = nil
+
+		redirect_to articles_url
+	end
 end
