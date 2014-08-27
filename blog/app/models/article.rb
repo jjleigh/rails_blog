e35a7b586c16f017801e50ce30a82fd16481c0ce
Justine Jones <justinejones91@gmail.com>
2014-08-27 19:58:10 -0400

    added another syntax error

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ActiveRecord::Base
 	has_many :comments 
 	has_many :users, :through => :comments
 
-	validates :title, presence: true,
-		validates	 :title, length: { minimum: 6 }
+	validates :title, presence: true
+	validates	:title, length: { minimum: 6 }
 end
