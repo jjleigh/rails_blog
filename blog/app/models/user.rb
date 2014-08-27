4b7be47327266900e4d18bc086a457fb05879b9c
Justine Jones <justinejones91@gmail.com>
2014-08-27 17:44:12 -0400

    added validations and fixed associations

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
-	has_many :articles
+	has_many :articles, :through => :comments
 	has_many :comments
+
+	has_many :articles
+
+	validates_presence_of :name, :email, :password
 end
