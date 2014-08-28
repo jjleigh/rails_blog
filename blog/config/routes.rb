25c4304949953b37d3e429cf76681f19d291596d
Justine Jones <justinejones91@gmail.com>
2014-08-28 13:26:20 -0400

    added an only condition the the nested article routes

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root :to => "articles#index"
   resources :users, :except => [:index] do
-    resources :articles
+    resources :articles, :only => [:show]
   end
   
 
