da06a46def96a2f66edace7d25d0f76ae29f77b7
Justine Jones <justinejones91@gmail.com>
2014-08-29 17:19:42 -0400

    added dependent destroy

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	has_many :articles, :through => :comments
 	has_many :comments
 
-	has_many :articles
+	has_many :articles :dependent :destrot
 
 	validates_presence_of :name, :email
 	validates :password, presence: true, length: { minimum: 6 }
