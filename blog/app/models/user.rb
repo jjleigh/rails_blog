91b68f69d82ee923983fe6bae21b85c452c90c67
Justine Jones <justinejones91@gmail.com>
2014-08-28 22:37:38 -0400

    added has secure password method which solved the unknown attribute error

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
+	has_secure_password
+	
 	has_many :articles, :through => :comments
 	has_many :comments
 
 	has_many :articles
 
-	validates_presence_of :name, :email, :password
+	validates_presence_of :name, :email
+	validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
