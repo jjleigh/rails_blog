f9de4b53b197447cf960135a011d221587f82f5a
Justine Jones <justinejones91@gmail.com>
2014-09-06 15:21:47 -0400

    changed a typo

--- blog/db/migrate/20140827214840_add_user_id_and_article_id_to_comment_table.rb
+++ blog/db/migrate/20140827214840_add_user_id_and_article_id_to_comment_table.rb
@@ -1,6 +1,6 @@
 class AddUserIdAndArticleIdToCommentTable < ActiveRecord::Migration
   def change
-  	add_column :comments, :user_id, :interger
-  	add_column :comments, :article_id, :interger
+  	add_column :comments, :user_id, :integer
+  	add_column :comments, :article_id, :integer
   end
 end
