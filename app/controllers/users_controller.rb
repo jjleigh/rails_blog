9d26aefc1e7b32911d56a07073c306d0cd98255e
Justine Jones <justinejones91@gmail.com>
2014-09-06 16:08:26 -0400

    changed something

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+  before_filter :ensure_logged_in, :only => [:edit, :update, :destroy, :show]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_session_path, :notice => "Thanks for Signing up!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+
+    @user.destroy
+    redirect_to articles_url
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    if current_user
+      @my_articles = @user.articles
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email,:description, :url, :password, :password_confirmation)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+	has_secure_password
+	
+	has_many :articles, :through => :comments
+	has_many :comments
+
+	has_many :articles, dependent: :destroy
+
+	validates_presence_of :name, :email
+	validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+end
