c8c1609ff2d295bc1094852192359f2004b3696e
Justine Jones <justinejones91@gmail.com>
2014-09-09 18:18:37 -0400

    do not need to log in to see a user'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :ensure_logged_in, :only => [:edit, :update, :destroy, :show]
+  before_filter :ensure_logged_in, :only => [:edit, :update, :destroy]
   def new
     @user = User.new
   end
